71a1de6848d519632f9778dbab2a511ebaa4c70d
LuA95 <luuchita1995@gmail.com>
2019-11-05 03:05:55 +0100

    [2W1-G08-LA]: Agrega TPI, Trello y Modelos

---
+++ 2W1/G08/ZooM/Zoom/Controllers/RecorridosController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zoom.Models;
+
+namespace Zoom.Controllers
+{
+    public class RecorridosController : Controller
+    {
+        // GET: Recorridos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            VMRecorridos vr = new VMRecorridos();
+            GestorPuntos gp = new GestorPuntos();
+            vr.Puntos = gp.ObtenerPuntos();
+            return View(vr);
+        }
+
+        [HttpPost]
+        public ActionResult Add(VMRecorridos vr)
+        {
+            GestorRecorridos gr = new GestorRecorridos();
+            gr.AgregarRecorrido(vr.Recorrido);
+            vr.Recorrido.Id=gr.ObtenerUltimoRecorrido();
+            gr.AgregarPPRecorrido(vr.Recorrido);
+            vr.Recorridos = gr.ObtenerRecorrido();
+            return View("List", vr);
+        }
+
+        public ActionResult List()
+        {
+            VMRecorridos vr = new VMRecorridos();
+            GestorRecorridos gr = new GestorRecorridos();
+            vr.Recorridos = gr.ObtenerRecorrido();
+            return View(vr);
+        }
+
+        public ActionResult Report()
+        {
+            VMReporte vr = new VMReporte();
+            return View(vr);
+        }
+
+        [HttpPost]
+        public ActionResult Report(VMReporte vr)
+        {
+            VMRecorridos recorridos = new VMRecorridos();
+            GestorRecorridos gr = new GestorRecorridos();
+            recorridos.Recorridos=gr.ObtenerRDisponibles(vr.Fecha_inicio,vr.Fecha_final);
+            return View("List", recorridos);
+        }
+    }
+}
---
+++ 2W1/G08/ZooM/Zoom/Models/Guia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zoom.Models
+{
+    public class Guia
+    {
+        int id;
+        string nombre;
+        string apellido;
+        string dni;
+
+        public Guia()
+        {
+        }
+
+        public Guia(int id, string nombre, string apellido, string dni)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Dni = dni;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Dni { get => dni; set => dni = value; }
+    }
+}

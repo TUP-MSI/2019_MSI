17ba46540513d3bd1626c1fc92535fe5256c090f
DefenderAndAttackHack <43101438+DefenderAndAttackHack@users.noreply.github.com>
2019-11-06 00:22:41 -0300

    Add files via upload

---
+++ 2W1/G04/Entregable/proyecto final ZOO/Metodologia/src/vista/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author feder
+ */
+public class Main {
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           
+      Inicio vp = new Inicio();
+        vp.setVisible(true);
+        vp.setLocationRelativeTo(null);
+        
+        
+      
+        
+        
+       
+    }
+    
+}
